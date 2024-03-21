8b2a0c6103c77160451e02f5e497aa9e88628d7c
tomi-casabona <tomas.casabona@gmail.com>
2024-03-21 20:56:51 +0100

    Ejercicios de testing tereminados, hasta el 8

--- src/films.js
+++ src/films.js
@@ -8,7 +8,7 @@ function getAllDirectors(array) {
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
   let result;
- result = array.filter((f) => f.director == director ? f : "");
+ result = array.filter( (f) => f.director == director ? f : "");
 return result;   
 } 
   
@@ -49,21 +49,63 @@ function orderByYear(movies) {
 }
 
 // Exercise 6: Calculate the average of the movies in a category
-function moviesAverageByCategory() {
-
-}
+function moviesAverageByCategory(movies, category) {
+  
+  let matchingItems = movies.filter( movie =>  movie.genre.includes(category) )
+ 
+   let accumulatedByCategory = matchingItems.reduce( (acumulador, movie) => {
+     return acumulador + movie.score;
+   }, 0);
+  
+ 
+ let averageByCategory = (accumulatedByCategory / matchingItems.length).toFixed(2)/1;
+    
+ return averageByCategory; 
+   
+ }; 
 
 // Exercise 7: Modify the duration of movies to minutes
-function hoursToMinutes() {
+function hoursToMinutes(movies) {
+    let moviesInMinutes = [];
+    moviesInMinutes = movies.map(movie => { // for each movie do ...
+    const durationParts = movie.duration.split(' '); // Pick the movie duration attribute and separte the parts of the string based from the Space
+    let totalMinutes = 0;  
+    // Calculate total minutes duration for each element
+    durationParts.forEach(part => { // for each part of the movie duration attribute
+      if (part.includes('h')) { //include the h part of the duration
+       parseInt(part) == 0 ? 0 : totalMinutes += parseInt(part) * 60; // change te h part to minutes and add to totalMinutes variable
+      } else if (part.includes('min')) {       //include the min part of the duration
+        totalMinutes += parseInt(part); // add the min part of the duration to the totalMinutes variable
+      }
+    });
 
-}
+    return { // from movies.map
+      ...movie,//spread operator for each movie object
+      duration: totalMinutes // Change the movie.duration attribute for each movie object
+    };
+  }
+  );
+  
+  return moviesInMinutes; // Return new array with movie duration in minutes
+
+};
+
+// sort By duration
+
+// let moviesChanged = hoursToMinutes(movies)  
+// moviesChanged.sort((a,b) => parseInt(a.duration) - parseInt(b.duration))
+// console.log(moviesChanged);
+
 
 // Exercise 8: Get the best film of a year
-function bestFilmOfYear() {
-  
+
+function bestFilmOfYear(movies, year) {
+
+  let yearMovies = movies.filter( (f) => f.year === year ? f : "") ;// filter films by year
+ 
+  let bestMovieOfYear= yearMovies.sort((a,b)=> a.score - b.score ); // order asc by score, and limit the array to 1
+  return bestMovieOfYear.slice(-1);
 }
-
-
 
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
--- tests/films.spec.js
+++ tests/films.spec.js
@@ -292,11 +292,52 @@ describe('Function "orderByYear"', () => {
 
 // Exercise 6
 // YOUR CODE HERE. Test moviesAverageByCategory()
+
+// Exercise 6
 describe('Function "moviesAverageByCategory"', () => {
-  it('ADD YOUR CODE IN films.spec.js file', () => {
-    expect(typeof hoursToMinutes).toBe('coffee');
+  it('should be declared', () => {
+    expect(typeof moviesAverageByCategory).toBe('function');
   });
+
+  it('should return a number', () => {
+    expect(typeof moviesAverageByCategory(movies, 'Crime')).toBe('number');
+  });
+
+  it('should be different from NaN', () => {
+    expect(moviesAverageByCategory(movies, 'Crime')).not.toBeNaN();
+  });
+
+  it(' should return the average score of movies selecting by genre. With 2 decimals! ', () => {
+    expect(moviesAverageByCategory([
+      {
+        title: 'Paths of Glory',
+        year: 1957,
+        director: 'Stanley Kubrick',
+        duration: '1h 28min',
+        genre: ['Drama', 'War'],
+        score: 8.4
+      },
+      {
+        title: 'Django Unchained',
+        year: 2012,
+        director: 'Quentin Tarantino',
+        duration: '2h 45min',
+        genre: ['Drama', 'Western'],
+        score: 8.4
+      },
+      {
+        title: 'Pulp Fiction',
+        year: 1994,
+        director: 'Quentin Tarantino',
+        duration: '2h 34min',
+        genre: ['Crime', 'Drama'],
+        score: 8.9
+      }
+    ], 'Crime')).toBe(8.90);
+  });
+
 });
+
 
 // Exercise 7
 describe('Function "hoursToMinutes"', () => {
@@ -386,3 +427,6 @@ describe('Function "bestFilmOfYear"', () => {
   });
 
 });
+
+
+
